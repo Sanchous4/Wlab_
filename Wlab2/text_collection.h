5f64ce5d5c7f29a40106129e4ff7731dec776c53
Sanchous4 <74542871+Sanchous4@users.noreply.github.com>
2020-11-16 18:11:21 +0300

    Update text_collection.h

--- Wlab2/text_collection.h
+++ Wlab2/text_collection.h
@@ -43,7 +43,7 @@ public:
     void push_stack(text_collection& add_stack);
     void push_operation(stack_obj& object, text_collection& change);
     void push_object(stack_obj& object);
-    void undo(text_collection& stackRedo);
+    void undo(text_collection& stack_cmback);
     void remove();
     void remove_all();
 
